9473662621a0982daada2789edc14a72395f5a50
suresh-reddy0810 <pernatisureshreddy2002@gmail.com>
2026-03-13 11:29:06 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -13,12 +13,16 @@ module "vnet" {
     sub2_name = var.sub2_name
     sub2_cicdr = var.sub2_cicdr
     nsg_name = var.nsg_name
+    rg_name = module.rg.rg_name
+    location = module.rg.location
 }
 
 module "vm" {
     source = "../../modules/vm/"
     vm_name = var.vm_name
     nic_name = var.nic_name
+    rg_name = module.rg.rg_name
+    location = module.rg-location
 
 }
 
@@ -36,4 +40,5 @@ module "webapp" {
 module "aks" {
     source = "../../modules/aks/"
     aks_name = var.aks_name
+
 }
